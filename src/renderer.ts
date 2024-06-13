bc6505a55f167edce3222944980c88f3d3b69fa7
17521187214 <fanfangloveyou@gmail.com>
2024-06-13 11:52:15 +0800

    0613pub

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,7 @@
 /**
  * This file will automatically be loaded by vite and run in the "renderer" context.
  * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
+ * Electron, visit
  *
  * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
  *
